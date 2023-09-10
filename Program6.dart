d1275419a52c0fa787210a8a671e3f9b9bd4c8df
kajalkuwar <97589308+kajalkuwar@users.noreply.github.com>
2023-09-10 22:45:23 +0530

    Create Program6.dart

---
+++ Program6.dart
@@ -0,0 +1,24 @@
+void main(){
+ var i = 5;
+ if(i > 5){
+   print("$i is greater than 5");
+ }
+ else if(i==0) {
+   print("zero");
+ }
+  else if(i==1) {
+   print("one");
+ }
+  else if(i==2) {
+   print("two");
+ }
+  else if(i==3) {
+   print("three");
+ }
+  else if(i==4) {
+   print("four");
+ }
+  else if(i==5) {
+   print("five");
+ }
+}
