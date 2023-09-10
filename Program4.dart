a73cb193a9db77a260c05a76f92ff6bcd224e7a2
kajalkuwar <97589308+kajalkuwar@users.noreply.github.com>
2023-09-10 22:36:30 +0530

    Create Program4.dart

---
+++ Program4.dart
@@ -0,0 +1,13 @@
+//take a number and print whether it is positive or negative.
+void main(){
+ int i = -1;
+ if(i > 0){
+   print("$i is a positive number");
+ }
+ else if(i == 0){
+   print("$i is a zero");
+ }
+ else {
+    print("$i is a negative number");
+ }
+}
