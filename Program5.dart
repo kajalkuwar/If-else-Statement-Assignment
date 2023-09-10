46736665aca5d42a15f261a02b5470032ca0aa0d
kajalkuwar <97589308+kajalkuwar@users.noreply.github.com>
2023-09-10 22:40:09 +0530

    Create Program5.dart

---
+++ Program5.dart
@@ -0,0 +1,10 @@
+//dart program to check if a character is a vowel orconsonant.
+void main(){
+ var ch = "j";
+ if(ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u'){
+   print("$ch is a consonant.");
+ }
+ else{
+   print("$ch is a vowel.");
+ }
+}
