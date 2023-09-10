6b97012969048e342e7b5c78f5beb57867b5bd81
kajalkuwar <97589308+kajalkuwar@users.noreply.github.com>
2023-09-10 22:31:57 +0530

    Create Program3.dart

---
+++ Program3.dart
@@ -0,0 +1,10 @@
+//program to determine if the user can cast a vote or not.
+void main(){
+ int age = 17;
+ if(age >= 18){
+   print("You can cast a vote");
+ }
+ else {
+    print("You can’t cast a vote.");
+ }
+}
