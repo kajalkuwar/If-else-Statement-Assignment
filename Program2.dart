10fe9374d5025f80c22d464675dceef0d104443c
kajalkuwar <97589308+kajalkuwar@users.noreply.github.com>
2023-09-10 22:28:47 +0530

    Add files via upload

---
+++ Program2.dart
@@ -0,0 +1,13 @@
+//Take a number and print whether it is lessthan 10 or greater than 10.
+void main(){
+ var i = 9;
+ if(i < 10){
+   print("$i is less than 10");
+ }
+  else if(i > 10) {
+     print("$i is greater than 10");
+  }
+   else {
+    print("$i is equal to 10");
+ }
+}
